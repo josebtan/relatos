d459f946b9c2bc4453ea8b5b7aea077bd3f1269d
josebtan <168875869+josebtan@users.noreply.github.com>
2025-04-03 19:50:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -403,5 +403,15 @@ async function init() {
   loadPosts();
   window.addEventListener('scroll', handleScroll);
 }
-
+const darkModeToggle = document.getElementById("dark-mode-toggle");
+if(darkModeToggle) {
+  darkModeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    if (document.body.classList.contains("dark-mode")) {
+      darkModeToggle.textContent = "Modo Claro";
+    } else {
+      darkModeToggle.textContent = "Modo Oscuro";
+    }
+  });
+}
 init();
